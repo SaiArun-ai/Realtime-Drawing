6cf7e2b63fb2a2af99135e3e115f086a741dd7f4
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-07-30 20:13:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-var r;
-var g;
-var b;
-function draw(){
-    
-    document.getElementById("Footer").style.color = "white";
-
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(500,500);
+    canvas = createCanvas(300,300);
+    canvas.position(600,200);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+function modelLoaded(){
+    console.log('No error');
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
 }
