841eb91ea6b7469d6674ad03cfd12e935e9e3b65
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-08-02 20:16:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var difference;
+var nosex;
+var nosey;
 function setup(){
     video = createCapture(VIDEO);
     video.size(500,500);
@@ -9,8 +12,20 @@ function setup(){
 function modelLoaded(){
     console.log('No error');
 }
+function draw(){
+    noStroke();
+    background("#FFD700");
+    fill("#C0C0C0");
+    square(nosex,nosey,difference);
+    document.getElementById("holdresult").innerHTML = "The side length of the square is" + floor(difference) + "px";
+}
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        difference = results[0].pose.leftWrist.x - results[0].pose.rightWrist.x;
+        nosex = results[0].pose.nose.x;
+        nosey = results[0].pose.nose.y;
+        console.log(nosex);
+        console.log(nosey);
     }
 }
